6a9b3ec3033c3f6af7159075bf0dff1dfcbb1416
yutoj <yutoogata4@gmail.com>
2019-07-06 23:24:07 +0900

    add google fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
 import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <h1>Hello World</h1>
+    </>
   );
 }
 
