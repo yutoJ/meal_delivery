391ec9937e8e621c4a59b1536758e396f50c6fa0
yutoj <yutoogata4@gmail.com>
2019-07-14 12:16:00 +0900

    add banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import GlobalStyle from "./styles/GlobalStyle";
 import { Navbar } from "./Navbar/Navbar";
+import { Banner } from "./Banner/Banner";
 
 function App() {
   return (
     <>
       <GlobalStyle/>
       <Navbar />
+      <Banner />
     </>
   );
 }
---
+++ src/Banner/Banner.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  height: 300px;
+  background-image: url(img/pizza.jpg);
+  background-position: center;
+  background-size: cover;
+  filter: contrast(70%);
+`;
+
